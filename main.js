08eca6b8cd4b218ab9fc0f192937100eb06e170d
rushayhays <rushayhays@outlook.com>
2022-02-17 11:32:12 -0600

    Final/last draft of working project

---
+++ main.js
@@ -0,0 +1,47 @@
+//Here's a program for plant identification
+
+function getReadyForWalk() {
+    console.log("Get ready for walk:")
+    console.log("shoes...check")
+    console.log("coat...check")
+    console.log("phone...check")
+}
+
+function goOnWalk(){
+    console.log("Heading out!")
+    console.log("Following the usual path")
+}
+
+
+function spotPlant() {
+    console.log("Oh, look a new and interesting plant!")
+}
+
+function observePlant() {
+    console.log("What interesting petal color and leaf shapes it has!")
+}
+
+function recordPlant() {
+    console.log("I will take a picture of it so that I can look it up")
+}
+
+function returnHome() {
+    console.log("Time to head home")
+    
+}
+function identifyPlant() {
+    console.log("My identify this app allows me to quickly identify any new plants I find with just a picture!")
+}
+
+function researchPlant() {
+    console.log("Now that I know what it is, I can find cool facts about it so that I remember it better!")
+}
+
+getReadyForWalk()
+goOnWalk()
+spotPlant()
+observePlant()
+recordPlant()
+returnHome()
+identifyPlant()
+researchPlant()
